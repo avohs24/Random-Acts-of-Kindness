6c806582c55d645726d80481a5918754eb564eff
Alex Vohs <Alex Vohs>
2017-07-10 13:13:30 -0400

    updated user model

--- models/users.js
+++ models/users.js
@@ -1,13 +1,67 @@
 module.exports = function(sequelize, DataTypes){
-  var User = sequelize.define("User",{
-    firstName: {DataTypes.STRING, allowNull: false},
-    lastName: {DataTypes.STRING, allowNull: false},
-    username: {DataTypes.STRING, allowNull: false, unique: true},
-    password: {DataTypes.STRING, allowNull: false},
-    creditCard: {DataTypes.INTEGER, allowNull: true, isCreditCard: true},
-    streetAddress: {DataTypes.STRING, allowNull: false},
-    state: {DataTypes.STRING, allowNull: false},
-    zip: {DataTypes.INTEGER, allowNull: false},
-
-  })
+  var user = sequelize.define("User",{
+    id: {
+      type: Datatypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [1, 50]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    creditCard: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isCreditCard: true
+    },
+    streetAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 2]
+      }
+    },
+    zip: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return user; 
 }
