3b2ff7570446c30345a34a1ca281129bb78c215c
Tiffany Hyatt <tiffany@Hyatt.local>
2017-07-15 16:39:32 -0400

    created signin, signup and index react component

--- app/components/Sign_up.js
+++ app/components/Sign_up.js
@@ -5,14 +5,69 @@ import OrgList from '../containers/orgs_list'
 export default class SignUp extends Component {
   render() {
     return (
-      <div>
-        <h1>SignUp Page</h1>
-        <div>
-          <input placeholder='username'></input>
-          <input placeholder='password'></input>
-        </div>
-        <div>
-          <button>submit</button>
+            <div className="container">
+
+        <div id="signuppage" className="signupcontainer animated bounceInRight">
+          <div className="row">
+            <form id="signupform" className="col m12">
+              <div className="row">
+                <div className="input-field col m6">
+                  <input placeholder="First Name" id="first_name" type="text" className="validate" />
+                  <label for="first_name">First Name</label>
+                </div>
+                <div className="input-field col m6">
+                  <input placeholder="Last Name" id="last_name" type="text" className="validate" />
+                  <label for="last_name">Last Name</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col m12">
+                  <input placeholder="Street Address" id="address" type="text" className="validate" />
+                  <label for="address">Street Address</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col m4">
+                  <input placeholder="City" id="city" type="text" className="validate" />
+                  <label for="city">City</label>
+                </div>
+                <div className="input-field col m4">
+                  <input placeholder="State" id="state" type="text" className="validate" />
+                  <label for="state">State</label>
+                </div>
+                <div className="row">
+                  <div className="input-field col m4">
+                    <input placeholder="Zip Code" id="zip-code" type="text" className="validate" />
+                    <label for="zip-code">Zip Code</label>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col m12">
+                  <input id="signuppassword" type="password" className="validate" />
+                  <label for="signuppassword">Password</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col m12">
+                  <input id="confirmpassword" type="password" className="validate" />
+                  <label for="confirmpassword">Confirm Password</label>
+                </div>
+              </div>
+              <div className="row center">
+                <div className="col m6">
+                  <button id="loginsubmit" className="btn waves-effect waves-light" type="submit" name="action">Submit
+              <i className="material-icons right">send</i>
+              </button>
+                </div>
+                <div className="col m6">
+                  <button id="resetbutton" className="btn waves-effect waves-light" onclick="resetFunc" value="Reset Form" name="action">Reset
+              <i className="material-icons right">send</i>
+              </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
