7c3a89a5a111b5e726277b61f1b1365094f94c0c
Tiffany Hyatt <tiffany@Hyatt.home>
2017-07-17 18:22:41 -0400

    fixed the sign in buttons and layout

--- app/components/Sign_in.js
+++ app/components/Sign_in.js
@@ -5,40 +5,36 @@ import OrgList from '../containers/orgs_list'
 export default class SignIn extends Component {
   render() {
     return (
-          <div id="loginpg" className=" center logincontainer animated bounceInDown">
+          <div id="loginpg" className="center logincontainer animated bounceInDown">
             <div className="row">
-              <form id="loginform" className="col s12">
+              <form id="loginform" className="col m12">
                 <div className="row">
-                  <div className="input-field col s6">
+                  <div className="input-field col m12">
                     <i className="material-icons prefix">person</i>
                     <textarea id="icon_prefix2" className="materialize-textarea"></textarea>
-                    <label for="icon_prefix2">Username</label>
+                    <label htmlFor="icon_prefix2">Username</label>
                   </div>
                 </div>
-              </form>
-            </div>
             <div className="row">
-              <form className="col s12">
-                <div className="row">
-                  <div className="input-field col s6">
+                  <div className="input-field col m12">
                     <i className="material-icons prefix">lock outline</i>
                     <input id="password" type="password" className="validate" />
-                    <label for="password">Password</label>
+                    <label htmlFor="password">Password</label>
                   </div>
                 </div>
-                <div className="row center">
-                  <div className="col m4">
+                <div className="row">
+                  <div className="col m6">
                     <button id="loginsubmit" className="btn waves-effect waves-light" type="submit" name="action">Submit
-              <i className="material-icons right">send</i>
-              </button>
-                  </div>
-                  <div className="col m4">
-                    <button id="resetbutton" className="btn waves-effect waves-light" onclick="resetFunc" value="Reset Form" name="action">Reset
-              <i className="material-icons right">send</i>
-              </button>
-                  </div>
+                    <i className="material-icons right">send</i>
+                    </button>
                 </div>
-              </form>
+                <div className="col m6">
+                  <button id="resetbutton" className="btn waves-effect waves-light" value="Reset Form" type="reset"  name="action">Reset
+                  <i className="material-icons right">send</i>
+                  </button>
+                </div>
+              </div>
+            </form>
             </div>
           </div>
     );
