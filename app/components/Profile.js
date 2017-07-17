4fdabd3560f87240df0dc3882ddb85e5230ae68d
Tiffany Hyatt <tiffany@Hyatt.home>
2017-07-16 20:48:15 -0400

    fixed errors in profile.js

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react';
+import SearchBar from '../containers/search_bar';
 import ReactDOM from 'react-dom';
+import LogoImg from '../../public/assets/imgs/rak-logo.jpg';
 
 export default class Index extends Component {
     render() {
         return (
-          <div className="container" id="profile"><br>
+          <body className="profilebg"><br /><br />
+          <div className="container" id="profile">
             <div className="row">
               <div className="col m4">
-                <img className="left smallimg" src="../public/assets/imgs/placeholder-user-photo.png"></img>
                 <h2 className="left">My Profile<span id="welcomeuser"></span></h2>
               </div>
-              <div className="col m8">
-                <div className="right"><a class="waves-effect waves-light btn"><i class="material-icons left">payment</i>Donation History</a></div>
+              <div className="col m8"><br />
+                <div className="right"><a className="waves-effect waves-light btn"><i className="material-icons left">payment</i>Donation History</a></div>
               </div>
-            </div>
-            <p className="left">Name:&nbsp;<span id="profilename"></span><br><br><br>
-            Street Address:&nbsp;<span id="profileaddress"></span><br><br><br>
-            City:&nbsp;<span id="profilecity"></span><br><br><br>
-            State:&nbsp;<span id="profilestate"></span><br><br><br>
-            Zip Code:&nbsp;<span id="profilezip"></span><br><br><br></br></p>
-          </div>
+              </div>
+              <div className="row">
+              <div className="col m12">
+            <p className="left padding">Name:<span id="profilename"></span><br /><br /><br />
+            Street Address:<span id="profileaddress"></span><br /><br /><br />
+            City:<span id="profilecity"></span><br /><br /><br />
+            State:<span id="profilestate"></span><br /><br /><br />
+            Zip Code:<span id="profilezip"></span><br /><br /><br /><br /></p>
+
+</div>
+</div>
+</div>
+</body>
+        );
+    }
+}
