eebf670dc31beccf5702fb7f40a077938a39e3a2
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-07-17 11:37:54 -0400

    wire login

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,20 +1,32 @@
 import React, {Component} from 'react';
-import SearchBar from '../containers/search_bar'
-import OrgList from '../containers/orgs_list'
+import SearchBar from '../containers/search_bar';
+import OrgList from '../containers/orgs_list';
+import {Field, reduxForm } from 'redux-form';
 
-export default class SignIn extends Component {
+class SignIn extends Component {
+  renderField(field){
+    return(
+      <div className='form-group'>
+        <label>{field.label}</label>
+        <input
+          className = 'form-control'
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render() {
     return (
       <div id="loginpg" className=" center logincontainer animated bounceInDown">
         <div className="row">
           <form id="loginform" className="col s12">
-            <div className="row">
-              <div className="input-field col s6">
-                <i className="material-icons prefix">person</i>
-                <textarea id="icon_prefix2" className="materialize-textarea"></textarea>
-                <label for="icon_prefix2">Username</label>
-              </div>
-            </div>
+            <Field
+              label = 'Username'
+              name = 'username'
+              component={this.renderField}
+            />
           </form>
         </div>
         <div className="row">
@@ -44,3 +56,7 @@ export default class SignIn extends Component {
     );
   }
 }
+
+export default reduxForm({
+  form: 'PostNewForm'
+})(SignIn);
