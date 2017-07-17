343a83ce7c647b82f128c2709f9ad0cb30d3c8ba
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-07-17 12:51:07 -0400

    wire Login Fields

--- app/components/Login.js
+++ app/components/Login.js
@@ -5,15 +5,24 @@ import {Field, reduxForm } from 'redux-form';
 
 class SignIn extends Component {
   renderField(field){
+
     return(
-      <div className='form-group'>
-        <label>{field.label}</label>
-        <input
-          className = 'form-control'
-          type="text"
-          {...field.input}
-        />
-      </div>
+        // <div className="row">
+        //   <div className="input-field col s6">
+        //     <i className="material-icons prefix">person</i>
+        //     <textarea id="icon_prefix2" className="materialize-textarea"></textarea>
+        //     <label htmlFor="icon_prefix2">{field.label}</label>
+        //   </div>
+        // </div>
+        <div className='input-field col s6'>
+          <i className="material-icons prefix">{field.icon}</i>
+          <label htmlFor={field.htmlFor}>{field.label}</label>
+          <input
+            className = {field.className}
+            type={field.type}
+            {...field.input}
+          />
+        </div>
     )
   }
 
@@ -22,22 +31,22 @@ class SignIn extends Component {
       <div id="loginpg" className=" center logincontainer animated bounceInDown">
         <div className="row">
           <form id="loginform" className="col s12">
+            {/* TODO: only hover on underline activates form */}
             <Field
-              label = 'Username'
-              name = 'username'
+              label = "Username"
+              name = "username"
+              icon = 'person'
+              htmlFor="icon_prefix2"
+              className = 'validate'
               component={this.renderField}
             />
-          </form>
-        </div>
-        <div className="row">
-          <form className="col s12">
-            <div className="row">
-              <div className="input-field col s6">
-                <i className="material-icons prefix">lock outline</i>
-                <input id="password" type="password" className="validate"/>
-                <label for="password">Password</label>
-              </div>
-            </div>
+            <Field
+              label = "Password"
+              name = "password"
+              htmlFor="password"
+              icon = 'lock outline'
+              component={this.renderField}
+            />
             <div className="row center">
               <div className="col m4">
                 <button id="loginsubmit" className="btn waves-effect waves-light" type="submit" name="action">Submit
@@ -53,9 +62,44 @@ class SignIn extends Component {
           </form>
         </div>
       </div>
+      // <div id="loginpg" className=" center logincontainer animated bounceInDown">
+      //   <div className="row">
+      //     <form id="loginform" className="col s12">
+      //       <Field
+      //         label = 'Username'
+      //         name = 'username'
+      //         component={this.renderField}
+      //       />
+      //     </form>
+      //   </div>
+      //   <div className="row">
+      //     <form className="col s12">
+      //       <div className="row">
+      //         <div className="input-field col s6">
+      //           <i className="material-icons prefix">lock outline</i>
+      //           <input id="password" type="password" className="validate"/>
+      //           <label for="password">Password</label>
+      //         </div>
+      //       </div>
+      //       <div className="row center">
+      //         <div className="col m4">
+      //           <button id="loginsubmit" className="btn waves-effect waves-light" type="submit" name="action">Submit
+      //             <i className="material-icons right">send</i>
+      //           </button>
+      //         </div>
+      //         <div className="col m4">
+      //           <button id="resetbutton" className="btn waves-effect waves-light" onclick="resetFunc" value="Reset Form" name="action">Reset
+      //             <i className="material-icons right">send</i>
+      //           </button>
+      //         </div>
+      //       </div>
+      //     </form>
+      //   </div>
+      // </div>
     );
   }
 }
+
 
 export default reduxForm({
   form: 'PostNewForm'
