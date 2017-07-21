b0dfe989f7aa94d907689a817b73ad914904c1b6
Tiffany Hyatt <tiffany@Hyatt.home>
2017-07-21 10:19:44 -0400

    added random donation component

--- app/components/Donation.js
+++ app/components/Donation.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import SearchBar from '../containers/search_bar'
-import OrgList from '../containers/orgs_list'
+import SearchBar from '../containers/search_bar';
+import OrgList from '../containers/orgs_list';
+import LogoImg from '../../public/assets/imgs/rak-logo.jpg';
+
 
 export default class Donation extends Component {
   render() {
     return (
       <div>
-        <h1>Donate</h1>
-        <div>
-          <input placeholder='username'></input>
-          <input placeholder='password'></input>
+        <div className="container donationcontainer">
+          <h2 className="headerbg padding center">Match Me</h2>
+          <p>
+            Based on your interests, we've selected the following charity as your first
+            choice. If you'd like to explore other choices please click on the right arrow, otherwise
+            click on the organization title to contribute.
+          </p>
+          <div id="randomcharity">
+          </div>
+          <div className="row">
+            <div className="col m6 padding">
+              <i className="material-icons left purplearrow">arrow_back</i>
+            </div>
+            <div className="col m6 padding">
+              <i className="material-icons right purplearrow">arrow_forward</i>
+            </div>
+          </div>
+          <div className="center">
+            <img className="smallimg" src={LogoImg} />
+          </div>
         </div>
-        <div>
-          <button>submit</button>
+        <div className="paddingtwo center">
+          <button id="charitydonate" className="btn waves-effect waves-light" type="submit" name="action">Donate
+              <i className="material-icons right">send</i>
+          </button>
         </div>
       </div>
     );
--- app/components/Index.js
+++ app/components/Index.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
 import LogoImg from '../../public/assets/imgs/rak-logo.jpg';
 import OrgList from '../containers/orgs_list';
-import {connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Login from './Login';
 // import { Link } from 'react-router';
 
 
 export default class Index extends Component {
-  constructor(props){
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {term: ''};
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = { term: '' };
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 
-  }
-  handleInput(event){
-    this.setState({term: event.target.value});
-  }
-  handleSubmit(event){
-    event.preventDefault();
-  }
+    }
+    handleInput(event) {
+        this.setState({ term: event.target.value });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+    }
     render() {
         return (
             <div className="container main">
@@ -30,9 +30,9 @@ export default class Index extends Component {
                     <div className="col s12 m5">
                         <div id="cellular">
                             <div className="center" id="cellular-inside">
-                                <Link to='/index'><img className="smallimg" src={LogoImg} /></Link>
+                                <Link to='/index'><img className="mediumimg" src={LogoImg} /></Link>
                                 <form onSubmit={this.handleSubmit}>
-                                     Interest: <br /><br />
+                                    Interest: <br /><br />
                                     <input id="category" type="text" className="Category" onChange={this.handleInput} value={this.state.term} /><br /><br /><br />
                                 </form>
                                 <Link id="viewcharity" className="btn waves-effect waves-light" type="submit" to='/organizations'>View Charities
@@ -42,17 +42,17 @@ export default class Index extends Component {
                             <div id="homebutton">
                             </div>
                         </div>
-                        </div>
+                    </div>
                     <div className="col s12 m7">
                         <div id="aboutus">
                             <h2>What Is RAK?</h2>
                             <p className="padding">Random Acts of Kindness (RAK) is an organization that allows you the user,
-                            to contribute to a random organization of your choice. You can think of us as a gatekeeper who ensures 
+                            to contribute to a random organization of your choice. You can think of us as a gatekeeper who ensures
                             that your funds go to the right people. We know that given the quantity of organizations that are in need
                             of your help, it can be difficult to choose one. This is where RAK comes in to help you decide. We collect
                             the categories that you're interested in donating to and randomly select one for you. This takes the guesswork,
                             pressure, and indecisiveness out of the equation. If you're ready to donate, we need you to sign up
-                            first by clicking on the sign up button. Once you're signed in, you can get started on your 
+                            first by clicking on the sign up button. Once you're signed in, you can get started on your
                             philanthropist journey. Thank you for your time and consideration into promoting a good cause.
                             </p>
                         </div>
@@ -63,9 +63,9 @@ export default class Index extends Component {
                                 </Link>
                             </div>
                             <div className="col m4">
-                              <Link id="signup" className="btn waves-effect waves-light" to='/signup'>Sign Up
+                                <Link id="signup" className="btn waves-effect waves-light" to='/signup'>Sign Up
                                  <i className="material-icons right">send</i>
-                              </Link>
+                                </Link>
                             </div>
                         </div>
                     </div>
